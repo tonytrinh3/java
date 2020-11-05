b32c2feff29000926e507370aaa3044d89058c72
tony trinh <tony.w.trinh3@gmail.com>
2020-11-04 18:45:03 -0800

    linkedlist

---
+++ LinkedList/src/Demo.java
@@ -0,0 +1,25 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Demo {
+    public static void main(String[] args) {
+        LinkedList<String> placesToVisit = new LinkedList<String>();
+        placesToVisit.add("Sydney");
+        placesToVisit.add("Melbourne");
+        placesToVisit.add("Brisbane");
+        placesToVisit.add("Perth");
+        placesToVisit.add("Canberra");
+        placesToVisit.add("Adelaide");
+        placesToVisit.add("Darwin");
+
+        printList(placesToVisit);
+    }
+
+    private static void printList(LinkedList<String> linkedList){
+        Iterator<String> i = linkedList.iterator();
+        while(i.hasNext()){
+            System.out.println("Now visting " + i.next());
+        }
+        System.out.println("==============================");
+    }
+}
